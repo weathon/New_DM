3811959ff2074b29b7cde9e6cf8eb64a80d4263b
栈堆储存器 <weathon>
2019-01-10 13:01:56 +0800

    Daily'

--- python/tree.py
+++ python/tree.py
@@ -21,4 +21,20 @@ def read(path):
       result.append(["dir",i])
   return result
 
+def add(father,name,file_or_dir):
+  a=file_or_dir
+
+
+def mkdir(path):
+ 
+	folder = os.path.exists(path)
+ 
+	if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
+		os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
+		print "---  new folder...  ---"
+		print "---  OK  ---"
+ 
+	else:
+		print "---  There is this folder!  ---"
+
 
