fef6a5afd10fdcc8400b26da2d9f4fd13f69dde5
Weathon <tcs06358@163.com>
2019-01-12 05:14:42 +0000

    Upload2

--- python/database.py
+++ python/database.py
@@ -3,23 +3,27 @@ import pymssql
 import pickle
 def set(ip,user,psd,database_name):
   try:
-    f=open("config.inf","w")
+    f=open("config.inf","wb")
     config=[ip,user,psd,database_name]
-    f.write(pickle.dump(config))
+    pickle.dump(config,f)
     f.close()
     return 0
-  except:
-    return -1 
+  except e:
+    return -1,e 
 
 def connect():
-
+  f=open("config.inf","rb")
+  ip,user,psd,database_name=pickle.load(f)
+  print(ip,user,psd,database_name)
+  f.close()
   conn = pymssql.connect(host='127.0.0.1',
                         user='',
                         password='123',
                         database='SQLTest',
                         charset='utf8')
-
-  #查看连接是否成功
+  return conn
+  u""" 
+#查看连接是否成功
   cursor = conn.cursor()
   sql = 'select * from student'
   cursor.execute(sql)
@@ -27,6 +31,6 @@ def connect():
   rs = cursor.fetchall()
 
   print(rs)
-
+"""
 if __name__=="__main__":
   print(eval(input()))
