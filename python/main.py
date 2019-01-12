b126568d39534867d4a3fe1c1fa6844350423aa8
weathon <tcs06358@163.com>
2019-01-12 19:02:11 +0000

    Add some Flask Script

--- python/main.py
+++ python/main.py
@@ -1,3 +1,20 @@
-import database
-import pic
+#import database
+#import pic
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Hello World!"
+
+@app.route('/action/connect/<ip>/<user>/<psd>/<database_name>')
+def action(ip,user,psd,name):
+    database.connect(ip,user,psd,name)
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
 
