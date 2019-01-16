28908c40fbc736f9b0825b6ea8b36f5f4fdbf9d7
weathon <tcs06358@163.com>
2019-01-16 21:40:51 +0000

    daily

---
+++ python/.password.py
@@ -0,0 +1,12 @@
+import hashlib
+
+def set_psd(salt,pro):
+    pass
+    
+
+def check_psd():
+   pass
+
+if __name__=="main":
+    print(eval(input()))
+    #For test, when run online, remove this part!
--- python/pic.py
+++ python/pic.py
@@ -1,4 +1,11 @@
 import base64
+import os
 
 #此处设计为整一个文件夹导入（无论扫描还是）
+def import_IMG(path):
+    files.listdir(path)
+    for i in files:
+        real_path=path+"/"+i
 
+def creat_PDF(path):
+    pass
