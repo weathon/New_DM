27b0a2141174d4273bb529c6655144ac6dcf8069
Wayne <tcs06358@163.com>
2019-01-28 17:46:03 -0600

    Update tree.py

--- python/tree.py
+++ python/tree.py
@@ -1,1 +1,19 @@
-
+def write(json_string):
+  try:
+      f=open('tree.json','w')
+      f.write(json_string)
+      f.close()
+      return 0
+  except:
+    return -1
+  
+  
+def read():
+  try:
+    f=open('tree.json','r')
+    json_string=f.read()
+    f.close()
+    return json_string
+  except:
+    return -1
+    
