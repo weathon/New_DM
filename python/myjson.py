ced30d01562bbe0cbdbf82507322a9a37a0a6456
tcs06358 <tcs06358@163.com>
2019-02-05 17:25:18 -0500

    Do something

--- python/myjson.py
+++ python/myjson.py
@@ -9,6 +9,15 @@ def list2json(lists,keys):
             thisline[keys[j]]=i[j]
         result.append(thisline)
     return result
+
+def readkeys():
+    f=open("keys.ini","r")#注意！一旦设置过keys更改可能会出错，还没有加入检测！！！
+    text=f.read()
+    f.close()
+    return text.split(",")#注意内容中不能有逗号
+#表头数据从数据库提取？
+
 if __name__=="__main__":
-    print(eval(input()))
+    while 1:
+        print(eval(input()))
         
