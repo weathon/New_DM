f1d9284d2876b265259c077439da6bd9bcbd8d72
Lab 25 <lab25@cf.local>
2019-02-12 16:29:19 -0600

    Chinese bug

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-import eel
-eel.init('./')
-eel.start('index.html')
+﻿import eel
+
 
 @eel.expose
 def helloworld(a, b):
     print(a, b, a + b)
+    return [
+                {
+                    id: 1,
+                    label: "安徽省",
+                    children: [
+                        {
+                            id: 2,
+                            label: "马鞍山市",
+                            disabled: true,
+                            children: [
+                                {
+                                    id: 3,
+                                    label: "和县",
+                                },
+                                {
+                                    id: 4,
+                                    label: "花山区",
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    id: 5,
+                    label: "河南省",
+                    children: [
+                        {
+                            id: 6,
+                            label: "郑州市"
+                        }
+                    ]
+                }
+            ]
+
+eel.init('./')
+eel.start('index.html')
