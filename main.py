92e5c1a09e2d4ab1b8032efbe434da7847329fd5
Lab 25 <lab25@cf.local>
2019-02-13 16:16:16 -0600

    -'

--- main.py
+++ main.py
@@ -2,41 +2,14 @@
 
 
 @eel.expose
-def helloworld(a, b):
-    print(a, b, a + b)
-    return [
+def getD():
+    return '''[
                 {
                     id: 1,
-                    label: "安徽省",
-                    children: [
-                        {
-                            id: 2,
-                            label: "马鞍山市",
-                            disabled: true,
-                            children: [
-                                {
-                                    id: 3,
-                                    label: "和县",
-                                },
-                                {
-                                    id: 4,
-                                    label: "花山区",
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    id: 5,
-                    label: "河南省",
-                    children: [
-                        {
-                            id: 6,
-                            label: "郑州市"
-                        }
-                    ]
+                    label: "",
+                    children: []
                 }
-            ]
+            ]'''
 
 eel.init('./')
 eel.start('index.html')
