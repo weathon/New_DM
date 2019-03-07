e6afad3ddba7c98b72450414e23275859505c215
LAB 11 <lab11@cf.local>
2019-03-07 15:20:14 -0600

    -

--- python/main.py
+++ python/main.py
@@ -85,39 +85,44 @@ def welcome():
     print("Welcome")
     # For sure can open two ws://
 
-@eel.expose
-def read_table(name):
-    name=str(name)
-    name=database2.check_dangerous(name)
-    mylist=database2.runsql("SELECT * FROM "+name)
-    keys=myjson.readkeys()
-    myjson_data=myjson.list2json(mylist)
-    # print(keys)
-    return_table='''<table class="layui-table" lay-data="{height:315, url:'/demo/table/user/', page:true, id:'test'}" lay-filter="test">
-        <thead>
-            <tr>
-            <th lay-data="{field:key1,sort: true}">%s</th>
-            <th lay-data="{field:key2,sort: true}">%s</th>
-            <th lay-data="{field:key3,sort: true}">%s</th>
-            <th lay-data="{field:key4,sort: true}">%s</th>
-            <th lay-data="{field:key5,sort: true}">%s</th>
-            <th lay-data="{field:key6,sort: true}">%s</th>
-            <th lay-data="{field:key7,sort: true}">%s</th>
-            <th lay-data="{field:key8,sort: true}">%s</th>
-            <th lay-data="{field:key9,sort: true}">%s</th>
-            <th lay-data="{field:key10,sort: true}">%s</th>
-            <th lay-data="{field:key11,sort: true}">%s</th>
-            <th lay-data="{field:key12,sort: true}">%s</th>
-            <th lay-data="{field:key13,sort: true}">%s</th>
-            <th lay-data="{field:key14,sort: true}">%s</th>
-            <th lay-data="{field:key15,sort: true}">%s</th>
-            </tr>
-        </thead>
-        </table>''' % tuple(keys)
+# @eel.expose
+# def read_table(name):
+#     name=str(name)
+#     name=database2.check_dangerous(name)
+#     mylist=database2.runsql("SELECT * FROM "+name)
+#     keys=myjson.readkeys()
+#     myjson_data=myjson.list2json(mylist)
+#     # print(keys)
+#     return_table='''<table class="layui-table" lay-data="{page:true, id:'main_table',data:'''+str(myjson_data)+''',height:315}" lay-filter="test">
+#         <thead>
+#             <tr>
+#             <th lay-data="{field:key1,sort: true}">%s</th>
+#             <th lay-data="{field:key2,sort: true}">%s</th>
+#             <th lay-data="{field:key3,sort: true}">%s</th>
+#             <th lay-data="{field:key4,sort: true}">%s</th>
+#             <th lay-data="{field:key5,sort: true}">%s</th>
+#             <th lay-data="{field:key6,sort: true}">%s</th>
+#             <th lay-data="{field:key7,sort: true}">%s</th>
+#             <th lay-data="{field:key8,sort: true}">%s</th>
+#             <th lay-data="{field:key9,sort: true}">%s</th>
+#             <th lay-data="{field:key10,sort: true}">%s</th>
+#             <th lay-data="{field:key11,sort: true}">%s</th>
+#             <th lay-data="{field:key12,sort: true}">%s</th>
+#             <th lay-data="{field:key13,sort: true}">%s</th>
+#             <th lay-data="{field:key14,sort: true}">%s</th>
+#             <th lay-data="{field:key15,sort: true}">%s</th>
+#             </tr>
+#         </thead>
+#         </table>''' % tuple(keys)
     
-    print(return_table)
-    return return_table
+    
+    # print(return_table)
+    # return return_table
 
+@eel.expose
+def get_cols():
+    keys=myjson.readkeys()
+    return keys
 
 eel.init('../html')
 eel.start('index.html', options={'chromeFlags': ['--disable-http-cache','--incognito']})
--- python/runsql.py
+++ python/runsql.py
@@ -1,5 +1,24 @@
 import sqlite3
+# import pymssql
+# def connect():
+#   f=open("config.inf","rb")
+#   ip,user,psd,database_name=pickle.load(f)
+#   print(ip,user,psd,database_name)
+#   f.close()
+#   conn = pymssql.connect(host='127.0.0.1',
+#                         user='',
+#                         password='123',
+#                         database='SQLTest',
+#                         charset='utf8')
+#   #return conn
+ 
+# #   cursor = conn.cursor()
+# #   sql = 'select * from student'
+# #   cursor.execute(sql)
+# #     rs = cursor.fetchall()
 
+# #   print(rs)
+# connect()
 def runsql(sql):
       database_name="main"
       conn = sqlite3.connect(database_name+".db")
@@ -17,6 +36,25 @@ def runsql(sql):
 print("-----------------------------------------------")
 print(runsql('select * from sqlite_master where type="table";'))
 print("-----------------------------------------------")
+# print(runsql('''CREATE TABLE test (
+# PATH nvarchar,
+# ID int,
+# key1 nvarchar,
+# key2 nvarchar,
+# key3 nvarchar,
+# key4 nvarchar,
+# key5 nvarchar,
+# key6 nvarchar,
+# key7 nvarchar,
+# key8 nvarchar,
+# key9 nvarchar,
+# key10 nvarchar,
+# key11 nvarchar,
+# key12 nvarchar,
+# key13 nvarchar,
+# key14 nvarchar,
+# key15 nvarchar
+# )'''))
 while 1:
       print(runsql(input(">")))
 # INSERT INTO test (key1,key2,key3,key4,key5,key6,key7,key8,key9,key10,key11,key1,key13,key14,key15) VALUES ("test","test","test","test","test","test","test","tst","test","test","test","test","test","test","test")
