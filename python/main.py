5ca4881c81e4431d0bc0692b8c057dd40b832504
weathon <tcs06358@163.com>
2019-03-07 15:43:41 -0600

    Do sth.

--- python/main.py
+++ python/main.py
@@ -85,40 +85,6 @@ def welcome():
     print("Welcome")
     # For sure can open two ws://
 
-# @eel.expose
-# def read_table(name):
-#     name=str(name)
-#     name=database2.check_dangerous(name)
-#     mylist=database2.runsql("SELECT * FROM "+name)
-#     keys=myjson.readkeys()
-#     myjson_data=myjson.list2json(mylist)
-#     # print(keys)
-#     return_table='''<table class="layui-table" lay-data="{page:true, id:'main_table',data:'''+str(myjson_data)+''',height:315}" lay-filter="test">
-#         <thead>
-#             <tr>
-#             <th lay-data="{field:key1,sort: true}">%s</th>
-#             <th lay-data="{field:key2,sort: true}">%s</th>
-#             <th lay-data="{field:key3,sort: true}">%s</th>
-#             <th lay-data="{field:key4,sort: true}">%s</th>
-#             <th lay-data="{field:key5,sort: true}">%s</th>
-#             <th lay-data="{field:key6,sort: true}">%s</th>
-#             <th lay-data="{field:key7,sort: true}">%s</th>
-#             <th lay-data="{field:key8,sort: true}">%s</th>
-#             <th lay-data="{field:key9,sort: true}">%s</th>
-#             <th lay-data="{field:key10,sort: true}">%s</th>
-#             <th lay-data="{field:key11,sort: true}">%s</th>
-#             <th lay-data="{field:key12,sort: true}">%s</th>
-#             <th lay-data="{field:key13,sort: true}">%s</th>
-#             <th lay-data="{field:key14,sort: true}">%s</th>
-#             <th lay-data="{field:key15,sort: true}">%s</th>
-#             </tr>
-#         </thead>
-#         </table>''' % tuple(keys)
-    
-    
-    # print(return_table)
-    # return return_table
-
 @eel.expose
 def get_cols():
     keys=myjson.readkeys()
