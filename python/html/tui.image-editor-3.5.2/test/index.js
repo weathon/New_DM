dc95b4f337a3fef22e0a4de549898b382b2c2c8a
weathon <tcs06358@163.com>
2019-06-23 17:04:20 -0500

    -

--- python/html/tui.image-editor-3.5.2/test/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview Test env
- * @author NHN Ent. FE Development Lab <dl_javascript@nhnent.com>
- */
-import '../src';
-
-const testsContext = require.context('.', true, /spec\.js$/);
-testsContext.keys().forEach(testsContext);
